bdaee0e8f54af30b842fd15739740aabedd20eb2
Misha Sharma <148586686+mishasharmaa@users.noreply.github.com>
2024-05-03 15:21:16 -0400

    Delete src/java directory

--- src/java/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.w24csci2020uassignment03hossinzehifrankweatherssharma;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
